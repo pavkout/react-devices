d8d49ae63069619b9adaa475eb201027363fa72c
Pavkout <pkoutoglou@gmail.com>
2016-12-10 16:03:13 +0200

    Update index.js from src
    Update index.js from src

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export { default as IPad } from './components/iPad';
 export { default as IPhone } from './components/iPhone';
 export { default as MacBook } from './components/macbook';
 export { default as Tablet } from './components/tablet';
+export { default as WindowsPhone } from './components/windowsPhone';
