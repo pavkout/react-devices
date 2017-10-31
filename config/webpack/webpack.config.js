3a1c5e970b6829b5a5e6ba3fa35aaa452f3b571c
James Gatz <jamesgatz@Jamess-MacBook-Pro.local>
2017-10-31 21:19:24 +0200

    feat(update): Update packages to the latest

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -4,20 +4,50 @@ var webpack = require('webpack');
 var autoprefixer = require('autoprefixer');
 var aliases = require('./aliases');
 
-var isDebug = _.includes(process.argv, '--production') || _.includes(process.argv, '-p') ? false : true;
+var isDebug =
+  _.includes(process.argv, '--production') || _.includes(process.argv, '-p')
+    ? false
+    : true;
 
-console.log(isDebug ? 'Building your stuff...' : 'Compiling for production...')
+console.log(isDebug ? 'Building your stuff...' : 'Compiling for production...');
 // webpack.config.js
 if (process.env.NODE_ENV === 'development') {
-  var loaders = ['react-hot','babel-loader']
+  var loaders = ['react-hot', 'babel-loader'];
 } else {
-  var loaders = ['babel-loader']
+  var loaders = ['babel-loader'];
 }
+
+const jsRules = {
+  test: /.jsx?$/,
+  loader: 'babel-loader',
+  exclude: /node_modules/,
+  options: {
+    presets: ['es2015', 'react', 'stage-0'],
+    babelrc: false
+  }
+};
+
+const cssRules = {
+  test: /\.css$/,
+  exclude: /node_modules/,
+  use: [
+    {
+      loader: 'style-loader'
+    },
+    {
+      loader: 'css-loader'
+    },
+    {
+      loader: 'postcss-loader',
+      options: {
+        plugins: [autoprefixer]
+      }
+    }
+  ]
+};
 
 module.exports = {
   context: path.resolve(__dirname, '../../'),
-  debug: isDebug,
-
   // Better set this for development, because it creates source maps
   devtool: isDebug ? '#inline-source-map' : false,
   entry: {
@@ -30,32 +60,16 @@ module.exports = {
     publicPath: '/dist/'
   },
   resolve: {
-     extensions: ['', '.js', '.jsx'],
-     root: path.resolve(__dirname, '../../'),
-     alias: aliases
+    extensions: ['.js', '.jsx'],
+    modules: [path.resolve(__dirname, '../../'), 'node_modules'],
+    alias: aliases
   },
   devServer: {
     inline: true,
     port: 8080,
-    'content-base': '../../examples'
+    contentBase: '../../examples'
   },
   module: {
-    loaders: [{
-      test: /.jsx?$/,
-      loader: loaders,
-      exclude: /node_modules/,
-      query: {
-        presets: ["es2015", "react", "stage-0"]
-      },
-      babelrc: false
-    },
-    {
-      test: /\.css$/,
-      loader: 'style-loader!css-loader!postcss-loader'
-    }
-  ]
-  },
-  postcss: function () {
-    return [autoprefixer];
+    rules: [jsRules, cssRules]
   }
 };
